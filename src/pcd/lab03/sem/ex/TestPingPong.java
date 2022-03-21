3bc5180c3a520efae63d9af7bd94e69dca380241
Lorenzo Campanelli <lorenzo.campanelli3@studio.unibo.it>
2022-03-21 13:22:43 +0100

    Ex lab03 ping-pong

--- src/pcd/lab03/sem/ex/TestPingPong.java
+++ src/pcd/lab03/sem/ex/TestPingPong.java
@@ -1,5 +1,7 @@
 package pcd.lab03.sem.ex;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * Unsynchronized version
  * 
@@ -9,8 +11,13 @@ package pcd.lab03.sem.ex;
  */
 public class TestPingPong {
 	public static void main(String[] args) {
-		new Pinger().start();
-		new Ponger().start();	
+		Semaphore pingerNotifiedEvent = new Semaphore(0);
+		Semaphore pongerNotifiedEvent = new Semaphore(0);
+		new Pinger(pingerNotifiedEvent, pongerNotifiedEvent).start();
+		new Ponger(pingerNotifiedEvent, pongerNotifiedEvent).start();	
+		
+		// notifico al pinger che l'evento si è verificato per far partire lui per primo
+		pongerNotifiedEvent.release();
 	}
 
 }
